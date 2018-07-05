41f097121a752944e80cb5647f65f8c4ea7d59f2
juanzjck <juanzjck1996@gmail.com>
2018-07-05 17:59:27 -0500

    Bloques

---
+++ Scripts/Inteface/Bloques.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloques : MonoBehaviour {
+    public List<GameObject> BLOQUES;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
